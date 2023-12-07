1eff4e1ce8fe72c6fac6d8d20f8386c147bf1d5c
Mateus Celestino <celestinofelipe2019@gmail.com>
2023-12-07 14:52:44 -0300

    formulário

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -31,7 +31,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.numPreco = new System.Windows.Forms.NumericUpDown();
@@ -39,15 +38,15 @@
             this.label4 = new System.Windows.Forms.Label();
             this.numID = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnNovoItems = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.grbFormulario = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.numPreco)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numID)).BeginInit();
+            this.grbFormulario.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -64,7 +63,7 @@
             // 
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(8, 68);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(186, 185);
             this.listView1.TabIndex = 1;
@@ -73,44 +72,27 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(8, 47);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(186, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.numPreco);
-            this.panel1.Controls.Add(this.txtNome);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.numID);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(218, 32);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(230, 96);
-            this.panel1.TabIndex = 3;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(169, 69);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(173, 67);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(50, 21);
             this.button2.TabIndex = 8;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(2, 58);
+            this.label5.Location = new System.Drawing.Point(6, 56);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
@@ -119,16 +101,16 @@
             // 
             // numPreco
             // 
-            this.numPreco.Location = new System.Drawing.Point(5, 73);
-            this.numPreco.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numPreco.Location = new System.Drawing.Point(9, 71);
+            this.numPreco.Margin = new System.Windows.Forms.Padding(2);
             this.numPreco.Name = "numPreco";
             this.numPreco.Size = new System.Drawing.Size(77, 20);
             this.numPreco.TabIndex = 6;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(104, 29);
-            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNome.Location = new System.Drawing.Point(108, 27);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(116, 20);
             this.txtNome.TabIndex = 4;
@@ -137,7 +119,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(103, 14);
+            this.label4.Location = new System.Drawing.Point(108, 0);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
@@ -146,8 +128,8 @@
             // 
             // numID
             // 
-            this.numID.Location = new System.Drawing.Point(5, 30);
-            this.numID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.numID.Location = new System.Drawing.Point(9, 28);
+            this.numID.Margin = new System.Windows.Forms.Padding(2);
             this.numID.Name = "numID";
             this.numID.Size = new System.Drawing.Size(77, 20);
             this.numID.TabIndex = 4;
@@ -155,27 +137,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2, 15);
+            this.label3.Location = new System.Drawing.Point(6, 13);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(18, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "ID";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 2);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Formulario";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 69);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(108, 67);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(50, 21);
             this.button1.TabIndex = 0;
@@ -183,35 +155,36 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button3
+            // btnNovoItems
             // 
-            this.button3.Location = new System.Drawing.Point(231, 265);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(68, 24);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Novo items";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnNovoItems.Location = new System.Drawing.Point(218, 265);
+            this.btnNovoItems.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNovoItems.Name = "btnNovoItems";
+            this.btnNovoItems.Size = new System.Drawing.Size(68, 24);
+            this.btnNovoItems.TabIndex = 8;
+            this.btnNovoItems.Text = "Novo items";
+            this.btnNovoItems.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnApagar
             // 
-            this.button5.Location = new System.Drawing.Point(304, 265);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(68, 24);
-            this.button5.TabIndex = 10;
-            this.button5.Text = "Apagar";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnApagar.Location = new System.Drawing.Point(290, 265);
+            this.btnApagar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(68, 24);
+            this.btnApagar.TabIndex = 10;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnEditar
             // 
-            this.button4.Location = new System.Drawing.Point(376, 265);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(68, 24);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Editar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnEditar.Location = new System.Drawing.Point(362, 265);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(68, 24);
+            this.btnEditar.TabIndex = 11;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // label6
             // 
@@ -223,38 +196,55 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "Lista de Produtos";
             // 
+            // grbFormulario
+            // 
+            this.grbFormulario.Controls.Add(this.button2);
+            this.grbFormulario.Controls.Add(this.numID);
+            this.grbFormulario.Controls.Add(this.label5);
+            this.grbFormulario.Controls.Add(this.button1);
+            this.grbFormulario.Controls.Add(this.numPreco);
+            this.grbFormulario.Controls.Add(this.label3);
+            this.grbFormulario.Controls.Add(this.txtNome);
+            this.grbFormulario.Controls.Add(this.label4);
+            this.grbFormulario.Location = new System.Drawing.Point(206, 47);
+            this.grbFormulario.Name = "grbFormulario";
+            this.grbFormulario.Size = new System.Drawing.Size(237, 104);
+            this.grbFormulario.TabIndex = 13;
+            this.grbFormulario.TabStop = false;
+            this.grbFormulario.Text = "Formulario";
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 292);
+            this.ClientSize = new System.Drawing.Size(455, 308);
+            this.Controls.Add(this.grbFormulario);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnNovoItems);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form3";
             this.Text = "Gestas da Prima";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Load += new System.EventHandler(this.Form3_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numPreco)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numID)).EndInit();
+            this.grbFormulario.ResumeLayout(false);
+            this.grbFormulario.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.GroupBox grbFormulario;
 
         #endregion
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label4;
@@ -262,9 +252,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown numPreco;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnNovoItems;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button2;
     }
--- Form3.cs
+++ Form3.cs
@@ -12,6 +12,39 @@ namespace listBox
 {
     public partial class Form3 : Form
     {
+        List<Produto> lista_produtos = new List<Produto>
+{
+    new Produto { Id = 1, Nome = "Chocolate", Preco = 19.9m },
+    new Produto { Id = 2, Nome = "Vinho", Preco = 29.5m },
+    new Produto { Id = 3, Nome = "Flores", Preco = 15.0m },
+    new Produto { Id = 4, Nome = "Cartão de Aniversário", Preco = 5.99m },
+    new Produto { Id = 5, Nome = "Cesta de Frutas", Preco = 25.0m },
+    new Produto { Id = 6, Nome = "Perfume", Preco = 49.99m },
+    new Produto { Id = 7, Nome = "Caneca Personalizada", Preco = 12.5m },
+    new Produto { Id = 8, Nome = "Bolo Decorado", Preco = 34.99m },
+    new Produto { Id = 9, Nome = "Chá Especial", Preco = 9.0m },
+    new Produto { Id = 10, Nome = "Kit de Velas Perfumadas", Preco = 18.75m },
+    new Produto { Id = 11, Nome = "Livro de Poesias", Preco = 22.8m },
+    new Produto { Id = 12, Nome = "Bouquet de Balões", Preco = 8.5m },
+    new Produto { Id = 13, Nome = "Kit de Massagem", Preco = 42.0m },
+    new Produto { Id = 14, Nome = "Caneta de Luxo", Preco = 30.25m },
+    new Produto { Id = 15, Nome = "Café Gourmet", Preco = 14.99m },
+    new Produto { Id = 16, Nome = "Kit de Maquiagem", Preco = 55.0m },
+    new Produto { Id = 17, Nome = "Almofada Personalizada", Preco = 16.5m },
+    new Produto { Id = 18, Nome = "Queijo Especial", Preco = 27.75m },
+    new Produto { Id = 19, Nome = "Relógio Decorativo", Preco = 38.0m },
+    new Produto { Id = 20, Nome = "Jogo de Toalhas Bordadas", Preco = 19.0m },
+    new Produto { Id = 21, Nome = "Conjunto de Taças", Preco = 32.5m },
+    new Produto { Id = 22, Nome = "Caixa de Bombons Finos", Preco = 23.49m },
+    new Produto { Id = 23, Nome = "Cesta de Café da Manhã", Preco = 44.99m },
+    new Produto { Id = 24, Nome = "Kit de Plantas Ornamentais", Preco = 28.0m },
+    new Produto { Id = 25, Nome = "Porta-Retrato Personalizado", Preco = 10.0m },
+    new Produto { Id = 26, Nome = "Sabonetes Artesanais", Preco = 13.25m },
+    new Produto { Id = 27, Nome = "Kit de Jogos de Tabuleiro", Preco = 36.75m },
+    new Produto { Id = 28, Nome = "Garrafa de Champagne", Preco = 49.5m },
+    new Produto { Id = 29, Nome = "Kit de Pintura", Preco = 20.99m },
+    new Produto { Id = 30, Nome = "Copo Térmico", Preco = 15.0m }
+};
         public Form3()
         {
             InitializeComponent();
@@ -58,5 +91,56 @@ namespace listBox
         {
 
         }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                int id = int.Parse(item.Text);
+
+                Produto produto = lista_produtos.Find(prod => prod.Id == id);
+
+                numID.Value = produto.Id;
+                txtNome.Text = produto.Nome;
+                numPreco.Value = produto.Preco;
+
+                // Para o laço de repetição.
+                break;
+            }
+
+            if (numID.Value == 0)
+                return;
+
+            // Mostrar o formulário
+            grbFormulario.Visible = true;
+
+            // Foca no campo Nome
+            txtNome.Focus();
+
+            // Desabilitar o botão Novo Item
+            btnNovoItems.Enabled = false;
+            btnEditar.Enabled = false;
+            btnApagar.Enabled = false;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        { 
+            ConfiguraListView();
+        }
+
+        void ConfiguraListView()
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("ID");
+            listView1.Columns.Add("Nome");
+            listView1.Columns.Add("Preço");
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            FechaFormulario();
+        }
     }
-}
+    }
+
